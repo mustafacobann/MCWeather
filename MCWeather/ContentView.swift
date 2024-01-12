4b2b852e4f769491635957a6df30f6ecb8d1562c
Mustafa Çoban <cobanium@icloud.com>
2024-01-12 10:41:36 +0300

    Create project

---
+++ MCWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MCWeather
+//
+//  Created by Mustafa on 12.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("MCWeather")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MCWeather/MCWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCWeatherApp.swift
+//  MCWeather
+//
+//  Created by Mustafa on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MCWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
